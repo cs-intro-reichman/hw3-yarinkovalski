d611e6e1e2e42710c7c63edc7dd510cb9f85b626
Yarin Kovalski <yarin.kovalski@post.runi.ac.il>
2024-11-23 10:31:43 +0200

    second try

--- LoanCalc.java
+++ LoanCalc.java
@@ -21,18 +21,16 @@ public class LoanCalc {
 		System.out.println("number of iterations: " + iterationCounter);
 
 		// Computes the periodical payment using bisection search
-		System.out.print("\nPeriodical payment, using bi-section search:` ");
+		System.out.print("\nPeriodical payment, using bi-section search: ");
 		System.out.println((int) bisectionSolver(loan, rate, n, epsilon));
 		System.out.println("number of iterations: " + iterationCounter);
-
-		System.out.println(endBalance(loan, rate, n, 10000));
 	}
 
 	// Computes the ending balance of a loan, given the loan amount, the periodical
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) 
 	{
-			
+		
 		for (int i=1; i<=n; i++)
 		{
 			loan = (loan-payment)*(1.0+(rate/100));
@@ -50,6 +48,7 @@ public class LoanCalc {
 	{
 		// Replace the following statement with your code
 		double g = loan/n;
+		iterationCounter=0;
 		while (endBalance(loan, rate, n, g)>0) 
 		{
 			g = g+epsilon;
@@ -65,8 +64,44 @@ public class LoanCalc {
 	// Given: the sum of the loan, the periodical interest rate (as a percentage),
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
-    public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-        // Replace the following statement with your code
-		return 0;
+    public static double bisectionSolver(double loan, double rate, int n, double epsilon)
+	{  
+		
+		double l = 0;
+		double h = loan;
+		double g = (l+h)/2;
+		iterationCounter=0;
+		
+		while ((h - l) >= epsilon )
+		{
+			
+			if ((endBalance(loan, rate, n, g))> 0)
+				l = g;
+			else
+				h = g;
+			g = (l + h)/2;	
+			iterationCounter++;
+		}
+		return g;
     }
+
+
+
+
+	       // Replace the following statement with your code
+							//// Sets L and H to initial values such that f(L) > 0, f(H) < 0,
+					//// implying that the function evaluates to zero somewhere between L and H.
+					// So, let’s assume that L and H were set to such initial values.
+					// Set g to (L + H)/2
+					//while (H − L) > ε {
+						// Sets L and H for the next iteration
+						//if f(g)∙f(L) > 0
+						// the solution must be between g and H
+						// so set L or H accordingly
+						//else
+						// the solution must be between L and g
+						// so set L or H accordingly
+						// Computes the mid-value (g) for the next iteration
+						//}
+					//	return g
 }
